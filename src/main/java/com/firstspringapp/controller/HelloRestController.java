39558412ae4b515586a761fbbd727255526477e2
iaksharhate <iaksharhate@gmail.com>
2022-07-08 22:13:23 +0530

    Added: PUT Request Method and pass first name as Path Variable and last name as Query Parameter.

--- src/main/java/com/firstspringapp/controller/HelloRestController.java
+++ src/main/java/com/firstspringapp/controller/HelloRestController.java
@@ -30,4 +30,10 @@ public class HelloRestController {
     public String sayHello(@RequestBody User user){
         return "Hello "+ user.getFirstName() + " " + user.getLastName() + "!";
     }
+    //curl -X PUT localhost:8080/hello/put/Lisa/?lastName=Terissa -w "\n"
+    @PutMapping("/put/{firstName}")
+    public  String sayHello(@PathVariable String firstName,
+                            @RequestParam(value = "lastName") String lastName){
+        return "Hello " + firstName + " " + lastName + "!";
+    }
 }
